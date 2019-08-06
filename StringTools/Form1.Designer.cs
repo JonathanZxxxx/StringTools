e2f8936c852dbb58c072c0f188460d2d630d7784
郑鑫23919 <zx23919@ly.com>
2019-08-06 11:26:55 +0800

    项目初始化

---
+++ StringTools/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace StringTools
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputLabel = new System.Windows.Forms.Label();
+            this.InputTextBox = new System.Windows.Forms.RichTextBox();
+            this.ExcuteButton = new System.Windows.Forms.Button();
+            this.OutputTextBox = new System.Windows.Forms.RichTextBox();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.SplitComboBox = new System.Windows.Forms.ComboBox();
+            this.SplitLabel = new System.Windows.Forms.Label();
+            this.SplitBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputLabel
+            // 
+            this.InputLabel.AutoSize = true;
+            this.InputLabel.Location = new System.Drawing.Point(23, 46);
+            this.InputLabel.Name = "InputLabel";
+            this.InputLabel.Size = new System.Drawing.Size(53, 12);
+            this.InputLabel.TabIndex = 0;
+            this.InputLabel.Text = "输入文本";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(92, 12);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(294, 108);
+            this.InputTextBox.TabIndex = 2;
+            this.InputTextBox.Text = "";
+            // 
+            // ExcuteButton
+            // 
+            this.ExcuteButton.Location = new System.Drawing.Point(55, 389);
+            this.ExcuteButton.Name = "ExcuteButton";
+            this.ExcuteButton.Size = new System.Drawing.Size(75, 23);
+            this.ExcuteButton.TabIndex = 3;
+            this.ExcuteButton.Text = "执行";
+            this.ExcuteButton.UseVisualStyleBackColor = true;
+            this.ExcuteButton.Click += new System.EventHandler(this.ExcuteButton_Click);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(92, 184);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(294, 110);
+            this.OutputTextBox.TabIndex = 4;
+            this.OutputTextBox.Text = "";
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Location = new System.Drawing.Point(23, 234);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(53, 12);
+            this.OutputLabel.TabIndex = 5;
+            this.OutputLabel.Text = "输出文本";
+            // 
+            // SplitComboBox
+            // 
+            this.SplitComboBox.FormattingEnabled = true;
+            this.SplitComboBox.Location = new System.Drawing.Point(497, 38);
+            this.SplitComboBox.Name = "SplitComboBox";
+            this.SplitComboBox.Size = new System.Drawing.Size(121, 20);
+            this.SplitComboBox.TabIndex = 6;
+            this.SplitComboBox.SelectedIndexChanged += new System.EventHandler(this.SplitComboBox_SelectedIndexChanged);
+            // 
+            // SplitLabel
+            // 
+            this.SplitLabel.AutoSize = true;
+            this.SplitLabel.Location = new System.Drawing.Point(429, 41);
+            this.SplitLabel.Name = "SplitLabel";
+            this.SplitLabel.Size = new System.Drawing.Size(53, 12);
+            this.SplitLabel.TabIndex = 7;
+            this.SplitLabel.Text = "切割字符";
+            // 
+            // SplitBox
+            // 
+            this.SplitBox.Location = new System.Drawing.Point(647, 37);
+            this.SplitBox.Name = "SplitBox";
+            this.SplitBox.Size = new System.Drawing.Size(100, 21);
+            this.SplitBox.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SplitBox);
+            this.Controls.Add(this.SplitLabel);
+            this.Controls.Add(this.SplitComboBox);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.ExcuteButton);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.InputLabel);
+            this.Name = "MainForm";
+            this.Text = "字符串处理小工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label InputLabel;
+        private System.Windows.Forms.RichTextBox InputTextBox;
+        private System.Windows.Forms.Button ExcuteButton;
+        private System.Windows.Forms.RichTextBox OutputTextBox;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.ComboBox SplitComboBox;
+        private System.Windows.Forms.Label SplitLabel;
+        private System.Windows.Forms.TextBox SplitBox;
+    }
+}
+
---
+++ StringTools/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringTools
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SplitBox.Hide();
+            var selectDic = new Dictionary<int, string>();
+            selectDic.Add(0, "换行符");
+            selectDic.Add(1, "空格");
+            selectDic.Add(3, "输入字符");
+            BindingSource bindingSource = new BindingSource();
+            bindingSource.DataSource = selectDic;
+            SplitComboBox.DataSource = bindingSource;
+            SplitComboBox.ValueMember = "Key";
+            SplitComboBox.DisplayMember = "Value";
+        }
+
+        private void ExcuteButton_Click(object sender, EventArgs e)
+        {
+            var a = InputTextBox.Text;
+            OutputTextBox.Text = a;
+        }
+
+        private void SplitComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var select = SplitComboBox.SelectedValue;
+            if (select.GetType().Name == "Int32" && (int)select == 3)
+            {
+                SplitBox.Show();
+            }
+        }
+    }
+}
